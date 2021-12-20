698a2994daaa8202840ecce6d0ada9ac94cf09ab
niranjaniitb <96410010+niranjaniitb@users.noreply.github.com>
2021-12-20 12:43:33 +0530

    Update score.py

--- score.py
+++ score.py
@@ -4,7 +4,7 @@ import streamlit as st
 from PIL import Image
 import requests
 from io import BytesIO
-import tensorflow as tf
+# import tensorflow as tf
 
 
 st.set_option("deprecation.showfileUploaderEncoding", False)
