afd45c053d82ab236e5b3f9e9ba0fd0a83120ca0
niranjaniitb <96410010+niranjaniitb@users.noreply.github.com>
2021-12-20 14:36:15 +0530

    Update score.py

--- score.py
+++ score.py
@@ -15,7 +15,7 @@ st.text("provide url")
 def load_model():
 # Model
 #   model = torch.hub.load('ultralytics/yolov5', 'yolov5s')  # or yolov5m, yolov5l, yolov5x, custom
-  model = torch.hub.load('', 'custom', source='local')  # or yolov5m, yolov5l, yolov5x, custom
+  model = torch.hub.load('', 'custom', source='local',force_reload=True)  # or yolov5m, yolov5l, yolov5x, custom
   
   return model
 
